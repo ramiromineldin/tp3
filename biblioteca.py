3c0426b8d9ab6506875ad8a45519f9802fe04547
Ramiro Mineldin <68164810+ramiromineldin@users.noreply.github.com>
2021-03-06 17:18:55 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,5 +1,7 @@
 from collections import deque
+import heapq
 from grafo import Grafo
+import random
 
 
 def camino_mas_corto(grafo, origen, final):
@@ -31,15 +33,53 @@ def bfs(grafo, origen, final):
 
 
 
-def page_rank(grafo, d):
+
+def diccionario_a_lista_ordenada(diccionario):
+    heap = []
+    lista = []
+    for item in list(diccionario.items()):
+        heapq.heappush(heap,item[::-1])
+    
+    for i in range(len(heap)):
+        lista.append(heapq.heappop(heap))
+    return lista[::-1]
+    
+    
+def page_rank(grafo, d, n):
     len_grafo = len(grafo)
     pageranks = {}
+    i = 0
     for vertice in grafo.vertices:
-        pageranks[vertice] = 1
+            pageranks[vertice] = 1.0/len_grafo
+    while (i < n):    
+        for vertice in grafo.vertices:
+            pagerank_sum = sum(pageranks[ady] / len(grafo.adyacentes(ady)) for ady in grafo.adyacentes(vertice))
+            pageranks[vertice] += ((1-d) / len_grafo + (d * pagerank_sum))
+        i+=1
+    
+    return diccionario_a_lista_ordenada(pageranks)
+     
+
+
+
+
+def random_walk(grafo, vertice, pagerank, n, largo):
     for vertice in grafo.vertices:
-        pagerank_sum = sum(pageranks[ady] / len(grafo.adyacentes(ady)) for ady in grafo.adyacentes(vertice))
-        pageranks[vertice] = (d / len_grafo + (1-d) * pagerank_sum)
-    return pageranks
+        for i in range(n):
+            for j in range(largo):
+                adyacentes = grafo.adyacentes(vertice)
+                random.choice(adyacentes)
+                
+
+
+
+def page_rank_personalizado(grafo, vertice, n):
+    pagerank = {}
+    for vertice in grafo.vertices:
+        pagerank[vertice] = 1
+    for v in grafo.adyacentes(vertice):
+        pagerank[v] *= 1/len(grafo.adyacentes(v))
+
 
 
 
@@ -48,11 +88,15 @@ g.agregar_vertice(1)
 g.agregar_vertice(2)
 g.agregar_vertice(3)
 g.agregar_vertice(4)
+g.agregar_vertice(5)
 g.agregar_arista(1,2,None)
 g.agregar_arista(2,3,None)
 g.agregar_arista(3,4,None)
+g.agregar_arista(3,5,None)
 print(len(g))
-print(page_rank(g, 0.85))
+print(page_rank(g, 0.8, 5))
+
+
 
 
 
