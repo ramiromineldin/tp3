2b6d49a0a74465a8cdfc2ae994485bd9e9752dc8
Ramiro Mineldin <68164810+ramiromineldin@users.noreply.github.com>
2021-03-11 22:41:20 -0300

    Update imprimir.py

--- imprimir.py
+++ imprimir.py
@@ -1,4 +1,6 @@
 def imprimir_camino(lista):
+    """Recibe una lista e imprime los elementos de esta separada por flechas y las distantas frases"""
+    
     frases = {0: "aparece en playlist", 1: "de", 2: "tiene una playlist", 3: "donde aparece"}
     a_imprimir = []
     num_frase = 0
@@ -20,11 +22,17 @@ def imprimir_camino(lista):
 
 
 def imprimir_flechas(lista):
+    """Recibe una lista e imprime los elementos de esta separada por flechas"""
+    
     print(" --> ".join(lista))
 
 def imprimir_puntocoma(lista):
+    """Recibe una lista e imprime los elementos de esta separada por punto y coma"""
+    
     print("; ".join(lista))
 
-def imprimir_numero(numero): 
-    print(numero)
+def imprimir_dato(dato): 
+    """Recibe un dato y lo imprime"""
+    
+    print(dato)
     
