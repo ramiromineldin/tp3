1a5cdb3004b532a7a21913a539160beb430628a5
Ramiro Mineldin <68164810+ramiromineldin@users.noreply.github.com>
2021-03-11 22:37:48 -0300

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -3,27 +3,36 @@ import random
 class Grafo:
 
     def __init__(self, es_dirigido):
+        """Constructor. Recibe un parametro de tipo Bool indicando si es dirigido o no"""
+        
         self.vertices = {}
         self.estado_es_dirigido = es_dirigido
         self.cantidad_aristas = 0
+        self.cantidad_vertices = 0
     
     def __len__(self):
-        return len(self.vertices)
+        """Devuelve la cantidad de vertices del grafo"""
+        return self.cantidad_vertices
 
     def __iter__(self):
+        """Itera todos los vertices del grafo"""
         return iter(self.vertices)
 
     def __str__(self):
+        """Representacion del grafo como un diccionario de diccionarios"""
         return str(self.vertices)
 
     def agregar_vertice(self, clave):
+        """Agrega un vertice al grafo. Si ya existia un vertice en el grafo con la misma clave devuelve False, True en caso contrario"""
         if clave in self.vertices:
            return False
         
         self.vertices[clave] = {}
+        self.cantidad_vertices +=1
         return True
     
     def borrar_vertice(self, clave):
+        """Borra un vertice del grafo y todas las aristas que lo conectan y devuelve True. En caso de que el vertice no se encuentre devuelve False"""
         if not clave in self.vertices: 
             return False
 
@@ -34,6 +43,8 @@ class Grafo:
         return True
 
     def agregar_arista(self, inicio, fin, peso):
+        """Agrega una arista al grafo que conecta los vertices pasados por parametro. En caso de agregarla correctamente devuelve True. 
+        Si la clave de inicio o fin no se encuentra en el grafo o si el peso de la arista es 0 devuelve Falso"""
         if not inicio in self.vertices or not fin in self.vertices or self.estan_unidos(inicio, fin) or peso == 0 or inicio == fin:
            return False
      
@@ -49,9 +60,12 @@ class Grafo:
         return True
 
     def total_aristas(self): 
+        """Devuelve la cantidad de aristas del grafo"""
         return self.cantidad_aristas
 
     def borrar_arista(self, inicio, fin):
+        """Borra una arista del grafo en caso de que se haya borrado correctamente devuelve True. 
+        En caso de que alguno los vertices pasados por parametro no pertenezcan al grafo devuelve False"""
         if not inicio in self.vertices or not fin in self.vertices:
            return False
         self.vertices[inicio].pop(fin)
@@ -61,21 +75,26 @@ class Grafo:
         return True
 
     def estan_unidos(self, inicio, fin):
+        """Devuelve True si los vertices pasados por parametro estan unidos por una arista. En caso de que alguno de los vertices no pertenezca al grafo devuelve False"""
         if not inicio in self.vertices or not fin in self.vertices:
            return False
         
         return fin in self.vertices[inicio]
 
     def peso_arista(self, inicio, fin):
-        if not inicio in self.vertices or not fin in self.vertices:
+        """Devuelve el peso de la arista que une a los vertices pasados por parametro y devuelve True. 
+        En caso que alguno de los vertices no se encuentre en el grafo o estos no esten unidos devuelve False"""
+        if not inicio in self.vertices or not fin in self.vertices or not estan_unidos(inicio, fin):
            return False
 
         return self.vertices[inicio][fin]
     
     def obtener_vertices(self):
+        """Devuelve una lista con los vertices del grafo"""
         return self.vertices.keys()
     
     def adyacentes(self, vertice):
+        """"Devuelve una lista con los vertices adyacentes del vertice pasado por parametro. En caso de que este no se encuentre en el grafo devuelve una excepcion""" 
         if not vertice in self.vertices:
             raise Exception("El vertice {} no se encuentra en el grafo".format(vertice))
         ady = list(self.vertices[vertice])
@@ -83,5 +102,8 @@ class Grafo:
 
 
     def vertice_aleatorio(self):
+        """Devuelve un vertice aleatorio del grafo. En caso de que el grafo este vacio devuelve None"""
+        if self.cantidad_vertices == 0:
+            return None
         return random.choice(list(self.vertices.keys()))
 
