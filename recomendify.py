5e133734e6e5f47b3c8021e034cc71afb3fe0049
nathencinoza <71742037+nathencinoza@users.noreply.github.com>
2021-03-11 22:10:37 -0300

    Update recomendify.py

--- recomendify.py
+++ recomendify.py
@@ -1,21 +1,31 @@
+#!/usr/bin/env python3
 import biblioteca
 import imprimir
 import sys
 import random
 
-def camino(canciones_usuarios, origen, final):
-    recorrido = biblioteca.camino_mas_corto(canciones_usuarios, origen, final)
-    if recorrido != None:
-        imprimir.imprimir_camino(recorrido)
+
+def camino(canciones_usuarios, origen, final, usuarios):
+    if origen in usuarios or final in usuarios: 
+        print("Tanto el origen como el destino deben ser canciones")
+
     else:
-        print("No se encontro el recorrido")
+        recorrido = biblioteca.camino_mas_corto(canciones_usuarios, origen, final)
+
+        if recorrido != None:
+            imprimir.imprimir_camino(recorrido)
+
+        else:
+            print("No se encontro el recorrido")
     return
 
-def mas_importantes(canciones_usuarios, usuarios, n):
-    pageranks = biblioteca.page_rank(canciones_usuarios, 0.85, 20)
+def mas_importantes(canciones_usuarios, usuarios, n, pagerank_ya_calculado):
+    if len(pagerank_ya_calculado) == 0:
+        pagerank_ya_calculado = biblioteca.page_rank(canciones_usuarios, 0.85, 20)
+
     canciones_mas_importantes = []
     tam_lista = 0
-    for elemento in pageranks:
+    for elemento in pagerank_ya_calculado:
         if tam_lista == n:
             break
         if not elemento in usuarios:
@@ -24,26 +34,29 @@ def mas_importantes(canciones_usuarios, usuarios, n):
     imprimir.imprimir_puntocoma(canciones_mas_importantes)
     return
         
-def recomendacion(canciones_usuarios, elemento,tipo, usuarios, n):
-    pageranks = biblioteca.page_rank_personalizado(canciones_usuarios, elemento, 15, 1000)
-
+def recomendacion(canciones_usuarios, elemento,tipo, usuarios, n,):
+    pageranks = biblioteca.page_rank_personalizado(canciones_usuarios, elemento, 100, 15)
+    print(len(pageranks))
     recomendacion = []
-    tam_lista = 0
     es_usuario = False
 
     if tipo == "usuarios":
         es_usuario = True
     
     for i in pageranks:
-        if tam_lista == n:
+        if len(recomendacion) == n:
             break
-        if (es_usuario and i in usuarios) or (not es_usuario and i not in usuarios):
+
+        if i == elemento: continue
+
+        elif (es_usuario and i in usuarios) or (not es_usuario and i not in usuarios):
             recomendacion.append(i)
-            tam_lista += 1
+
     imprimir.imprimir_puntocoma(recomendacion)
+
+    
     return
 
-    #REVISAR OBTENER CICLO EN BIBLIOTECA
 def ciclo_n_canciones(red_canciones, n, cancion):
     ciclo = biblioteca.obtener_ciclo(red_canciones, cancion, n)
     imprimir.imprimir_flechas(ciclo)
@@ -51,22 +64,26 @@ def ciclo_n_canciones(red_canciones, n, cancion):
 
 
 def rango(red_canciones, n, cancion):
-    return biblioteca.rango_n(red_canciones, cancion, n)
+    rango = biblioteca.rango_n(red_canciones, cancion, n)
+    imprimir.imprimir_numero(rango)    
 
 
 def clustering(red_canciones, cancion):
     if cancion:
-        return biblioteca.calcular_clustering(red_canciones, cancion)
-    return biblioteca.clustering_promedio(red_canciones)
+        coef = biblioteca.calcular_clustering(red_canciones, cancion)
+    else: 
+        coef = biblioteca.clustering_promedio(red_canciones)
+    coef = round(coef, 3)
+    imprimir.imprimir_numero(coef)
 
 
-def procesar_comando(comando, parametros, canciones_usuarios, canciones_por_playlist, usuarios, esta_creado):
+def procesar_comando(comando, parametros, canciones_usuarios, canciones_por_playlist, usuarios, red_canciones, pagerank_ya_calculado):
     if comando == "camino":
         canciones = parametros.split(" >>>> ")
-        camino(canciones_usuarios, canciones[0], canciones[1])
+        camino(canciones_usuarios, canciones[0], canciones[1], usuarios)
     
     elif comando == "mas_importantes":
-        mas_importantes(canciones_usuarios, usuarios, int(parametros))
+        mas_importantes(canciones_usuarios, usuarios, int(parametros), pagerank_ya_calculado)
     
     elif comando == "recomendacion":
         parametros_separados = parametros.split(" ", 2)
@@ -74,9 +91,10 @@ def procesar_comando(comando, parametros, canciones_usuarios, canciones_por_play
         n = parametros_separados[1]
         canciones = parametros_separados[2].split(" >>>> ")
         cancion = random.choice(canciones)
-        recomendacion(canciones_usuarios, cancion, tipo, usuarios, n)
+        recomendacion(canciones_usuarios, cancion, tipo, usuarios, int(n))
     
     elif comando == "ciclo" or comando == "rango":
+        es_ciclo = False
         if comando == "ciclo":
             es_ciclo = True
         parametros_separados = parametros.split(" ", 1)
@@ -84,9 +102,9 @@ def procesar_comando(comando, parametros, canciones_usuarios, canciones_por_play
         cancion = parametros_separados[1]
 
         if es_ciclo:
-            ciclo_n_canciones(red_canciones, n, cancion)
+            ciclo_n_canciones(red_canciones, int(n), cancion)
         else:
-            rango(red_canciones, n , cancion)
+            rango(red_canciones, int(n) , cancion)
 
     elif comando == "clustering":
        clustering(red_canciones, parametros)
@@ -97,11 +115,13 @@ def procesar_comando(comando, parametros, canciones_usuarios, canciones_por_play
 
 def procesar_entrada(canciones_usuarios, canciones_por_playlist, usuarios):
     esta_creado = False
+    pagerank_ya_calculado = []
     red_canciones = None
     for row in sys.stdin:
+        row = row.rstrip("\n")
         entrada = row.split(" ", 1)
         comando = entrada[0]
-        if len(entrada > 1):
+        if len(entrada) > 1:
             parametros = entrada[1]
         else:
             parametros = None
@@ -110,7 +130,7 @@ def procesar_entrada(canciones_usuarios, canciones_por_playlist, usuarios):
             red_canciones = biblioteca.grafo_crear_estructura_2(canciones_por_playlist)
             esta_creado = True
         
-        procesar_comando(comando, parametros, canciones_usuarios, canciones_por_playlist, usuarios, red_canciones)
+        procesar_comando(comando, parametros, canciones_usuarios, canciones_por_playlist, usuarios, red_canciones, pagerank_ya_calculado)
 
 
         
@@ -121,3 +141,5 @@ def main():
     canciones_por_playlist, red_canciones_usuarios, usuarios = biblioteca.grafo_crear_estructura(sys.argv[1])
 
     procesar_entrada(red_canciones_usuarios, canciones_por_playlist, usuarios)
+
+main()
